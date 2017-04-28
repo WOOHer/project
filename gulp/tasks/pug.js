f8046265e1a5f3e4f9e08712f34cf3ba99d46014
Oleg Bratukhin <wooher@inbox.ru>
2017-04-28 18:32:18 +0300

    editting 1 on comments

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -1,9 +1,14 @@
 'use strict';
 
+var fs = require('fs');//add string
+
 module.exports = function() {
+    let locals = JSON.parse(fs.readFileSync('./content.json', 'utf-8'));
   $.gulp.task('pug', function() {
     return $.gulp.src('./source/template/pages/*.pug')
-      .pipe($.gp.pug({ pretty: true }))
+      .pipe($.gp.pug({
+          locals: locals,//add string
+          pretty: true }))
       .on('error', $.gp.notify.onError(function(error) {
         return {
           title: 'Pug',
