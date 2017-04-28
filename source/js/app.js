a3f2ba12d0a8cb37485484855011ba91bd6ec204
Oleg Bratukhin <wooher@inbox.ru>
2017-04-28 17:36:38 +0300

    add project

--- source/js/app.js
+++ source/js/app.js
@@ -5,3 +5,15 @@
     document.querySelector('.greating_picture').classList.add('m--show');
   }, 1000);
 })();
+
+  //map
+ymaps.ready(init);
+var myMap;
+
+function init(){
+  myMap = new ymaps.Map('map', {
+    center: [58.605717, 49.665167],
+    zoom: 12,
+    controls: ['default','routeEditor','smallMapDefaultSet'],
+  });
+}
