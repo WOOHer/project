881d29672c80bbdc103c61981de8b208b7861de0
Oleg Bratukhin <wooher@inbox.ru>
2017-05-11 18:52:30 +0300

    start to make hamburger

--- source/js/app.js
+++ source/js/app.js
@@ -1,12 +1,12 @@
-(function() {
-  'use strict';
+// (function() {
+//   'use strict';
+//
+//   setTimeout(function() {
+//     document.querySelector('.greating_picture').classList.add('m--show');
+//   }, 1000);
+// })();
 
-  setTimeout(function() {
-    document.querySelector('.greating_picture').classList.add('m--show');
-  }, 1000);
-})();
-
-  //map
+//map
 ymaps.ready(init);
 var myMap;
 
@@ -17,3 +17,33 @@ function init(){
     controls: ['default','routeEditor','smallMapDefaultSet'],
   });
 }
+
+//paralax
+// var paralax = (function () {
+//   var bg = document.querySelector('.head__bg');
+//   var user = document.querySelector('.welcome__text');
+//
+//   return {
+//     move: function(block, windowScroll, strafeAmount) {
+//       var strafe = windowScroll / -strafeAmount + '%';
+//       var transformString = 'translate3d(0,' + strafe + ', 0)';
+//
+//       var style = block.style;
+//
+//       style.transform = transformString;
+//       style.wekitTransform = transformString;
+//
+//       style.top = strafe;
+//     },
+//
+//     init: function (wScroll) {
+//       this.move(bg, wScroll, 45);
+//     }
+//   }
+// }());
+//
+// window.onscroll = function () {
+//   var wScroll = window.pageYOffset;
+//
+//   paralax.init(wScroll);
+// }
